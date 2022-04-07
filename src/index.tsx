e90cdcd9e8450a59a5c701c5ed85e5d98e2ee112
greatstudentkaze <dfyz0r@list.ru>
2022-04-07 22:23:09 +0300

    remove redundant files, use createRoot method to init app

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// @ts-ignore
+import { createRoot } from 'react-dom/client';
 
 import App from './app';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
