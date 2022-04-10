802028f15d894d8262a4ec01816a9f987c82dd13
greatstudentkaze <dfyz0r@list.ru>
2022-04-10 23:37:20 +0300

    feat(styles): add base styles

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './app';
+import './styles/index.scss';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
